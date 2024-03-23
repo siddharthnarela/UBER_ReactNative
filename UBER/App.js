ea07e32af92176ae34e8f2f3a8e2e2c269c7d8fd
Siddharth Narela <74176439+siddharthnarela@users.noreply.github.com>
2024-03-23 18:12:26 +0530

    .

--- UBER/App.js
+++ UBER/App.js
@@ -3,8 +3,8 @@ import {Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      
       <StatusBar style="auto" />
     </View>
   );
